906a4f885d239ae4aa93de783e35e01f6a1545d3
Minh Quan Hoang <minh.q.hoang@mail.mcgill.ca>
2019-10-27 13:17:20 -0400

    startmenu created

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/gui/StartMenu.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse223.quoridor.gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class StartMenu extends Stage {
+	public StartMenu()
+	{
+		try
+		{
+			this.setScene(new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("StartMenu.fxml"))));
+		}
+		catch (IOException ex)
+		{
+			System.err.println(ex.getMessage());
+		}
+	}
+}
