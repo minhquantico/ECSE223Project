623510ab500bb344fe86b751cb8260203d5824c0
jakep13 <52505145+jakep13@users.noreply.github.com>
2019-11-04 12:38:18 -0500

    Removed Main class
    Run the app from  QuoridorApplication.java

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,8 +1,11 @@
 package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
+import fxml.MainController;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class QuoridorApplication {
+public class QuoridorApplication extends Application {
 
 	private static Quoridor quoridor;
 
@@ -10,6 +13,27 @@ public class QuoridorApplication {
 		if (quoridor == null)
 			quoridor = new Quoridor();
  		return quoridor;
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//instantiate the main controller for all the screens
+				MainController mainContainer = new MainController();
+			    mainContainer.loadScreen("StartGame", "resources/StartGame.fxml",  "resources/StartGame.css");
+			    mainContainer.loadScreen("thinkingScreen", "resources/Scene.fxml",  "resources/Scene.css");
+			   //mainContainer.loadScreen("SelectUsername", "resources/SelectUsername.fxml",  "resources/SelectUsername.css");
+			    mainContainer.loadScreen("PlayScreen", "resources/PlayScreen.fxml",  "resources/PlayScreen.css");
+			    mainContainer.loadScreen("WhiteSelectUsername", "resources/SelectWhitePlayerUsername.fxml", null);
+			    mainContainer.loadScreen("BlackSelectUsername", "resources/SelectBlackPlayerUsername.fxml", null);
+			    mainContainer.instance.setOnHidden(e -> System.exit(0));
+			    mainContainer.setScreen("StartGame");
+				mainContainer.show();
+				//this is true --> System.out.println(MainController.instance==mainContainer);
+		
+	}
+	public static void main(String[] args)
+	{
+		launch(args);
 	}
 
 }
