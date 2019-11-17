7170d71df63730ca5419a52a27c499f91fed6705
jakep13 <52505145+jakep13@users.noreply.github.com>
2019-11-16 19:04:39 -0500

    Set state machine test method
    Set sm test method

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/PawnBehaviour.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/PawnBehaviour.java
@@ -1331,5 +1331,76 @@ public class PawnBehaviour
 	  return false;
 	  
   } 
+  public void setSMTest(int row, int column){
+	//assuming it is already in active state
+
+	//longitudinal
+	if(row==8){
+	//—> near border South
+
+	setPawnSMGameActiveLongitudinalLongitudinalSouthBorder(PawnSMGameActiveLongitudinalLongitudinalSouthBorder.nearBorder);
+
+	}else if(row==2){
+
+	//—>near Border North
+	setPawnSMGameActiveLongitudinalLongitudinalNorthBorder(PawnSMGameActiveLongitudinalLongitudinalNorthBorder.nearBorder);
+
+	}else if(row==1){
+
+	//—> On border North
+
+	setPawnSMGameActiveLongitudinalLongitudinalNorthBorder(PawnSMGameActiveLongitudinalLongitudinalNorthBorder.OnBorder);
+
+	}else if(row==9){
+
+	//—> On border South
+
+	setPawnSMGameActiveLongitudinalLongitudinalSouthBorder(PawnSMGameActiveLongitudinalLongitudinalSouthBorder.OnBorder);
+
+	}else{
+	//—>is In Longitudinal.Between;
+
+	setPawnSMGameActiveLongitudinalLongitudinal(PawnSMGameActiveLongitudinalLongitudinal.Between);
+
+	}
+
+
+
+	//latitudinal
+
+	if(column==8){
+	//—> near border East
+	setPawnSMGameActiveLatitudinalLatitudinalEastBorder(PawnSMGameActiveLatitudinalLatitudinalEastBorder.nearBorder);
+
+
+	}else if(column==2){
+
+	//—>near Border West
+
+	setPawnSMGameActiveLatitudinalLatitudinalWestBorder(PawnSMGameActiveLatitudinalLatitudinalWestBorder.nearBorder);
+
+
+	}else if(column==1){
+
+	//—> On border West
+
+	setPawnSMGameActiveLatitudinalLatitudinalWestBorder(PawnSMGameActiveLatitudinalLatitudinalWestBorder.OnBorder);
+
+	}else if(column==9){
+
+	//—> On border East
+	setPawnSMGameActiveLatitudinalLatitudinalEastBorder(PawnSMGameActiveLatitudinalLatitudinalEastBorder.OnBorder);
+
+
+	}else{
+	//—>is In Latitudinal.Between;
+
+	setPawnSMGameActiveLatitudinalLatitudinal(PawnSMGameActiveLatitudinalLatitudinal.Between);
+
+	}
+
+
+	}
+
 
 }
