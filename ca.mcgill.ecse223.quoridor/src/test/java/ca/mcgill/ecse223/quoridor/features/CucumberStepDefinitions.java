c8e9e104b524c5a38148116233c5a074b5694c5f
Traian Coza <traian.coza@gmail.com>
2019-11-23 14:25:59 -0500

    Made game drawn

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/CucumberStepDefinitions.java
@@ -1636,37 +1636,41 @@ public class CucumberStepDefinitions {
 	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
 	    //
 	    // For other transformations you can register a DataTableType.
+		
+		List<Map<String, String>> valueMaps = dataTable.asMaps();
+		for (Map<String, String> map : valueMaps) {
+			Integer row = Integer.decode(map.get("row"));
+			Integer col = Integer.decode(map.get("col"));
+			
+			Controller.doPawnMove(col, row);;
+		}
+		
 	    throw new cucumber.api.PendingException();
 	}
 
 	@Given("Player {string} has just completed his move")
 	public void player_has_just_completed_his_move(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    // ??
 	}
 
 	@Given("The last move of {string} is pawn move to {int}:{int}")
 	public void the_last_move_of_is_pawn_move_to(String string, Integer int1, Integer int2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    Controller.doPawnMove(int2, int1);
 	}
 
 	@When("Checking of game result is initated")
 	public void checking_of_game_result_is_initated() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    Controller.detectDraw();
 	}
 
 	@Then("Game result shall be {string}")
 	public void game_result_shall_be(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    assertEquals(QuoridorApplication.getQuoridor().getCurrentGame().getGameStatus().equals(GameStatus.Draw), string.equals("Drawn"));
 	}
 
 	@Then("The game shall no longer be running")
 	public void the_game_shall_no_longer_be_running() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	    assertNotEquals(GameStatus.Running, QuoridorApplication.getQuoridor().getCurrentGame().getGameStatus());
 	}
 
 	@Given("The new position of {string} is {int}:{int}")
